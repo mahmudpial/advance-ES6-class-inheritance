bbd77c21c3e1fa09fcb2d3f6ce599a290ca8b459
mahmudpial <pial15-11856@diu.edu.bd>
2023-03-08 23:13:36 +0600

    filter and find js

---
+++ extends.js
@@ -0,0 +1,11 @@
+class Instructor {
+  name;
+  designaton = "Web course instructor";
+  team = "web team";
+  location;
+  constructor(name, location) {}
+  startSupportSession(time) {}
+  quizCreate(module) {}
+
+  provideFeedBack() {}
+}
---
+++ forEach.js
@@ -0,0 +1,12 @@
+const friends = ["pial", "mahmud", "hasan", "badol", "murgi"];
+
+friends.forEach((friend) => console.log(friend));
+
+const products = [
+  { id: 1, name: "laptop", price: 45000 },
+  { id: 1, name: "mobile", price: 5000 },
+  { id: 1, name: "macBook", price: 145000 },
+  { id: 1, name: "bike", price: 450000 },
+];
+
+products.forEach((product) => console.log(product.price));
