89a00371537ff8fc072a6a20b0bb98a11b08e103
mahmudpial <pial15-11856@diu.edu.bd>
2023-03-09 15:47:38 +0600

    class javascript

---
+++ class.js
@@ -0,0 +1,18 @@
+// syntactic sugar
+class Instructor {
+  name;
+  designation = "web course instructor";
+  team = "web team";
+  location;
+  constructor(name, location) {
+    this.name = name;
+    this.location = location;
+  }
+  startSupportSession(time) {
+    console.log(`start the support session at ${time}`);
+  }
+}
+
+const pial = new Instructor("pial", "Dhaka");
+console.log(pial);
+pial.startSupportSession("9.30 PM");
