110b878f7c8853d6161081dc47ec799168b19f65
mahmudpial <pial15-11856@diu.edu.bd>
2023-03-09 16:38:29 +0600

    class extends javascript

--- class.js
+++ class.js
@@ -16,3 +16,7 @@ class Instructor {
 const pial = new Instructor("pial", "Dhaka");
 console.log(pial);
 pial.startSupportSession("9.30 PM");
+
+const sakib = new Instructor("sakib", "mirpur");
+console.log(sakib);
+pial.startSupportSession("9.30 PM");
--- extends.js
+++ extends.js
@@ -1,11 +1,62 @@
-class Instructor {
+// syntactic sugar
+// parent class
+class TeamMember {
   name;
-  designaton = "Web course instructor";
-  team = "web team";
   location;
-  constructor(name, location) {}
-  startSupportSession(time) {}
-  quizCreate(module) {}
-
-  provideFeedBack() {}
+  team = " web team";
+  constructor(name, location) {
+    this.name = name;
+    this.location = location;
+  }
 }
+// child class extends parent class
+class Instructor extends TeamMember {
+  designation = "web course instructor";
+  constructor(name, location) {
+    super(name, location);
+  }
+  startSupportSession(time) {
+    console.log(`start the support session at ${time}`);
+  }
+}
+
+class Devloper extends TeamMember {
+  designation = "web course Instructor";
+  Tech;
+  constructor(name, location, Tech) {
+    super(name, location);
+    this.Tech = Tech;
+  }
+  DevloperFeature(feature) {
+    console.log(`please devlop the ${feature}`);
+  }
+  release(version) {
+    console.log(`please release the version ${version}`);
+  }
+}
+
+class JobPlacement extends TeamMember {
+  designation = "job placement comandos";
+  region;
+  constructor(name, location, region) {
+    super(name, location);
+    this.region = region;
+  }
+  ProvideResume(feature) {
+    console.log(`please devlop the ${feature}`);
+  }
+  preparestudent(version) {
+    console.log(`please release the version ${version}`);
+  }
+}
+
+const job = new JobPlacement("pial", "kolkhata", "India");
+console.log(job);
+
+const pial = new Instructor("pial", "Dhaka");
+console.log(pial);
+pial.startSupportSession("9.30 PM");
+
+const sakib = new Instructor("sakib", "mirpur");
+console.log(sakib);
+pial.startSupportSession("9.30 PM");
